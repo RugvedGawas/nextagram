50a75d4ccc5bbf53d45b996738f32c8e94a95494
Rugved Gawas <rugvedgawas0018@gmail.com>
2024-12-25 18:33:18 +0530

    yet another commit

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -6,12 +6,12 @@ const page = () => {
       <div className="w-full bg-white fixed top-0 z-10">
         <h1 className="text-4xl m-4">User Profile</h1>
       </div>
-      <div>
-        <img
-          src="https://cdn-icons-png.flaticon.com/128/456/456212.png"
-          alt=""
-        />
-      </div>
+
+      <img
+        src="https://cdn-icons-png.flaticon.com/128/456/456212.png"
+        alt=""
+        className="rounded-full w-10 h-10  flex items-center justify-center bg-white mt-20 shadow-xl m-4"
+      />
     </>
   );
 };
