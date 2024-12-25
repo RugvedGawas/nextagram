df299a202b2c18c41bd172ab1d3218a3af340d8e
Rugved Gawas <rugvedgawas0018@gmail.com>
2024-12-25 16:47:13 +0530

    initial commit

---
+++ app/profile/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <div className="w-full bg-white fixed top-0 z-10">
+        <h1 className="text-4xl m-4">User Profile</h1>
+      </div>
+      <div>
+        <img
+          src="https://cdn-icons-png.flaticon.com/128/456/456212.png"
+          alt=""
+        />
+      </div>
+    </>
+  );
+};
+
+export default page;
